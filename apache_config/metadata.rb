d0c4e191bc970c194d35dac773531beb1a791f9b
Dee Jordan <dee@red-staple.com>
2014-02-28 10:37:28 -0800

    adding back default recipe and metadata, this is a custom recipe becuase we are using a custom layer so apache recipe is never called by default

---
+++ apache_config/metadata.rb
@@ -0,0 +1,7 @@
+name             "apache2 config"
+description      "Installs apache conf file only"
+maintainer       "Beebox"
+version          "1.0.0"
+
+recipe "apache2_conf", "Installs apache conf file only"
+
---
+++ apache_config/recipes/default.rb
@@ -0,0 +1,13 @@
+template 'apache2.conf' do
+  case node[:platform]
+  when 'centos','redhat','fedora','amazon'
+    path "#{node[:apache][:dir]}/conf/httpd.conf"
+  when 'debian','ubuntu'
+    path "#{node[:apache][:dir]}/apache2.conf"
+  end
+  source 'apache2.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  # notifies :run, resources(:bash => 'logdir_existence_and_restart_apache2')
+end
